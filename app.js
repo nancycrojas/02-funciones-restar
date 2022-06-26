11424941b2806fe482419c67b3c1eb5c44c191b9
nancycrojas <nanclarisaok@gmail.com>
2022-06-26 19:01:54 -0300

    Funciones - ejercicio 02

---
+++ app.js
@@ -0,0 +1,15 @@
+//restar(a, b)
+//Crear una función restar que tome como argumentos dos números y devuelva la resta de ellos
+
+//restar(3, 2) // 1
+//restar(10, 5.5) // 4.5
+//restar(3, 5) // -2
+
+const restar = (a, b) => {
+//function restar (a, b) {
+    return a - b;
+}
+
+restar (3, 2);
+restar(10, 5.5);
+restar(3, 5);
